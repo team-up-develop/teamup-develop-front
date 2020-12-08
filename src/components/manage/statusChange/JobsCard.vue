a6acd9a8978bf4652fdea4d2f3e4211607b948ce
坂本一弥 <sskazuya1130@gmail.com>
2020-12-08 22:42:21 +0900

    案件ステータス管理画面作成

---
+++ src/components/manage/statusChange/JobsCard.vue
@@ -0,0 +1,145 @@
+<script lang="ts">
+import Vue from 'vue';
+import axios from 'axios';
+
+type DataType = {
+  applyNum: number;
+  rejectNum: number;
+  assginNum: number;
+}
+
+export default Vue.extend({
+  props: {
+    jobTitle: { type: String, default: "" },
+    jobId: { type: Number, default: 0 }
+  },
+  data(): DataType {
+    return {
+      applyNum: 0,
+      rejectNum: 0,
+      assginNum: 0
+    }
+  },
+  filters: {
+    //* 案件タイトル 詳細 文字制限
+    truncateDetailTitle: function(value: string) {
+      const length = 60;
+      const ommision = "...";
+      if (value.length <= length) {
+        return value;
+      }
+      return value.substring(0, length) + ommision;
+    },
+  },
+  created() {
+    axios.get(`http://localhost:8888/api/v1/apply_job/?job_id=${ this.jobId }&apply_status_id=1`)
+    .then(response => {
+      this.applyNum = response.data.length
+    })
+    axios.get(`http://localhost:8888/api/v1/apply_job/?job_id=${ this.jobId }&apply_status_id=3`)
+    .then(response => {
+      this.rejectNum = response.data.length
+    })
+    axios.get(`http://localhost:8888/api/v1/apply_job/?job_id=${ this.jobId }&apply_status_id=2`)
+    .then(response => {
+      this.assginNum = response.data.length
+    })
+  }
+})
+</script>
+<template>
+  <section>
+    <v-sheet class="card">
+      <v-col>
+        <v-row class="card__top">
+          {{ jobTitle | truncateDetailTitle }}
+        </v-row>
+        <v-row class="card__center">
+          <router-link :to="`/jobs/${ jobId }`"> 
+            <button class="detail-btn">詳細をみる</button>
+          </router-link>
+          <v-row class="data-area">
+            <div class="value">
+              <div class="num">{{ applyNum }}</div>
+              <label for="name">応募数</label>
+            </div>
+            <div class="value">
+              <div class="num">{{ assginNum }}</div>
+              <label for="name">参加数</label>
+            </div>
+            <div class="value">
+              <div class="num">{{ rejectNum }}</div>
+              <label for="name">拒否数</label>
+            </div>
+          </v-row>
+        </v-row>
+      </v-col>
+    </v-sheet>
+  </section>
+</template>
+
+
+<style lang="scss" scoped>
+@import '@/assets/scss/_variables.scss';
+section {
+  width: 30%;
+
+  @media screen and (max-width: 900px) {
+    width: 100%;
+  }
+
+}
+.card {
+  @include card-border-color;
+  width: 360px;
+  padding: 1rem 1.2rem ;
+  border-radius: 8px;
+
+  @media screen and (max-width: 900px) {
+    margin: 0 auto;
+    width: 85%;
+    margin-bottom: 1.5rem;
+  }
+
+  @media screen and (max-width: 600px) {
+    width: 95%;
+  }
+
+  @media screen and (max-width: 450px) {
+    width: 98%;
+  }
+
+  &__top {
+    padding: 0.2rem 0 0 0rem;
+    height: 90px;
+  }
+
+  &__center {
+    height: 120px;
+    position: relative;
+
+    .detail-btn {
+      @include box-shadow-btn;
+      @include purple-btn;
+      color: $basic-white;
+      padding: 0.5rem 4rem;
+      position: absolute;
+      left: 0;
+      width: 100%;
+      font-weight: bold;
+      font-size: 0.8em;
+    }
+    .data-area {
+      padding: 3.5rem 0 0 0;
+
+      .value {
+        width: 33%;
+
+        .num {
+          font-size: 1.5em;
+        }
+      }
+    }
+  }
+}
+</style>
--- src/views/manage/Reject.vue
+++ src/views/manage/Reject.vue
@@ -3,38 +3,27 @@ import Vue from 'vue';
 import axios from 'axios';
 import moment from "moment";
 import { ManageJob } from '@/types/manage';
-// import StatusChange from '@/components/manage/StatusChange'
+import JobsCard from '@/components/manage/statusChange/JobsCard.vue'
+import UserCard from '@/components/manage/statusChange/UserCard.vue'
 
 export type DataType = {
-  applyUsers: ManageJob[];
-  applyUsersNum: number;
-  assginUsers: ManageJob[];
-  assginUsersNum: number;
   rejectUsers: ManageJob[];
-  rejectUsersNum: number;
-  favoriteUsers: ManageJob[];
-  favoriteUsersNum: number;
-  manageJobs: ManageJob[];
   userId: number;
   jobTitle: string;
 }
 
 export default Vue.extend({
+  components: {
+    JobsCard,
+    UserCard
+  },
   props: {
     // * job.idを受け取る
     id: Number,
   },
   data(): DataType {
     return {
-      applyUsers: [], //? 応募者
-      applyUsersNum: 0,//? 応募者人数
-      assginUsers: [], //? 参加者
-      assginUsersNum: 0, //? 参加者人数
       rejectUsers: [], //? 拒否者
-      rejectUsersNum: 0, //? 拒否者人数
-      favoriteUsers: [], //? お気に入りしているユーザー一覧
-      favoriteUsersNum: 0, //? お気に入りしているユーザー 人数
-      manageJobs: [], //? 管理
       userId: this.$store.state.auth.userId,
       jobTitle: ""
     }
@@ -55,439 +44,120 @@ export default Vue.extend({
     },
   },
   created() {
-    // * 参加者をステータスごとに取り出す
-    axios.get(`http://localhost:8888/api/v1/apply_job/?job_id=${ this.id }&apply_status_id=1`)
-    .then(response => {
-      this.applyUsers = response.data
-      this.applyUsersNum = response.data.length
-    })
-    axios.get(`http://localhost:8888/api/v1/apply_job/?job_id=${ this.id }&apply_status_id=2`)
-    .then(response => {
-      this.assginUsers = response.data
-      this.assginUsersNum = response.data.length
-    })
     axios.get(`http://localhost:8888/api/v1/apply_job/?job_id=${ this.id }&apply_status_id=3`)
     .then(response => {
       this.rejectUsers = response.data
-      this.rejectUsersNum = response.data.length
-    })
-
-    axios.get(`http://localhost:8888/api/v1/favorite_job/?job_id=${ this.id }`)
-    .then(response => {
-      this.favoriteUsers = response.data
-      this.favoriteUsersNum = this.favoriteUsers.length
     })
   },
   mounted() {
-    // * 管理している案件を取得する
-    axios.get(`http://localhost:8888/api/v1/job/?user_id=${this.userId}`)
-    .then(response => {
-      this.manageJobs = response.data
-    })
     // *  案件タイトル取得
     axios.get(`http://localhost:8888/api/v1/job/${ this.id }`)
     .then(response => {
-      console.log(response.data.jobTitle)
       this.jobTitle = response.data.jobTitle
     })
-  },
-  methods: {
-    // * 参加者 リアルタイムで変更する
-    compliteAssgin(){
-      // * 参加者をステータスごとに取り出す
-      axios.get(`http://localhost:8888/api/v1/apply_job/?job_id=${ this.id }&apply_status_id=1`)
-      .then(response => {
-        this.applyUsers = response.data
-        this.applyUsersNum = response.data.length
-      })
-      axios.get(`http://localhost:8888/api/v1/apply_job/?job_id=${ this.id }&apply_status_id=2`)
-      .then(response => {
-        this.assginUsers = response.data
-        this.assginUsersNum = response.data.length
-      })
-    },
-    // *拒否者 リアルタイムで取得
-    compliteReject() {
-      // * 参加者をステータスごとに取り出す
-      axios.get(`http://localhost:8888/api/v1/apply_job/?job_id=${ this.id }&apply_status_id=1`)
-      .then(response => {
-        this.applyUsers = response.data
-        this.applyUsersNum = response.data.length
-      })
-      axios.get(`http://localhost:8888/api/v1/apply_job/?job_id=${ this.id }&apply_status_id=3`)
-      .then(response => {
-        this.rejectUsers = response.data
-        this.rejectUsersNum = response.data.length
-      })
-    }
-  },
-  components: {
-    // StatusChange
   }
 });
 </script>
 
 <template>
   <section>
-    <div class="back-space">
-      案件名:
-      <router-link :to="`/jobs/${ id }`"> 
-        <span>{{ jobTitle | truncateDetailTitle }}</span>
-      </router-link>
-    </div>
-    <div class="manage-wrapper">
-      <v-card class="job-manage-wrapper">
-        <router-link :to="`/manage/applicant/${ id }`" class="router-no-link">
-          <div class="manage-job-area">
-            <span>応募者 <font-awesome-icon icon="user" class="icon"/></span> 
-          </div>
-        </router-link>
-        <router-link :to="`/manage/participate/${ id }`" class="router-link">
-          <div class="apply-job-area">
-            <span>参加者 <font-awesome-icon icon="users" class="icon"/></span>
-          </div>
-        </router-link>
-        <router-link :to="`/manage/reject/${ id }`" class="router-link">
-          <div class="reject-job-area">
-            <span>拒否者 <font-awesome-icon icon="user-alt-slash" class="icon"/></span> 
-          </div>
-        </router-link>
-        <div class="title-area">名前</div>
-        <div class="time-area">学習開始日</div>
-        <div class="skill-area">スキル</div>
-        <div class="job-wrapper-area">
-          <router-link :to="`/manage/profile/${ id }/${ rejectUser.userId }`" v-for="rejectUser in rejectUsers" :key="rejectUser.id" class="router">
-            <div class="job-area">
-              <div class="job-area-box">
-                <span>{{ rejectUser.user.userName }}</span>
-                <p>{{ rejectUser.user.userName }}</p>
-              </div>
-              <div class="job-area-box">
-                <span>{{ rejectUser.user.learningStartDate | moment("YYYY年 M月 D日") }}</span>
-                <p>{{ rejectUser.user.learningStartDate | moment("YYYY年 M月 D日") }}</p>
-              </div>
-              <div class="job-area-box">
-                仮置き: {{ rejectUser.user.userName }}
-              </div>
-            </div>
-          </router-link>
-          </div>
-      </v-card>
-    </div>
+    <v-container class="wrapper">
+      <v-row>
+        <JobsCard 
+          :jobTitle="jobTitle" 
+          :jobId="id"
+        />
+        <v-sheet class="manage">
+          <v-row class="manage__header">
+            <router-link :to="`/manage/applicant/${ id }`" class="router-link">
+              <span>応募者</span> 
+            </router-link>
+            <router-link :to="`/manage/participate/${ id }`" class="router-link">
+              <span>参加者</span>
+            </router-link>
+            <router-link :to="`/manage/reject/${ id }`" class="router-link-active-click">
+              <span>拒否者</span> 
+            </router-link>
+          </v-row>
+          <v-col>
+            <router-link 
+              :to="`/manage/profile/${ id }/${ user.userId }`" 
+              v-for="user in rejectUsers" 
+              :key="user.id"
+              class="users"
+            >
+              <UserCard :user="user" />
+            </router-link>
+          </v-col>
+        </v-sheet>
+      </v-row>
+    </v-container>
   </section>
 </template>
 
 <style lang="scss" scoped>
 @import '@/assets/scss/_variables.scss';
 
-.router-link :hover {
-  opacity: 0.8 ;
+.wrapper {
+  width: 90%;
+  max-width: none;
+
+  @media screen and (max-width: 1100px) {
+    width: 97%;
+  }
 }
 
-.router-no-link, .router-link {
-  text-decoration: none;
-  // color: $basic-white;
-}
-
-.back-space {
-  margin-top: 1rem;
-  font-weight: bold;
-}
-
-.manage-wrapper {
-  width: 100%;
-  height: 89.5vh;
+.manage {
+  @include card-border-color;
+  width: 60%;
+  border-radius: 8px;
   margin: 0 auto;
+  background-color: #ffffff;
+  position: relative;
+  font-size: 14px;
+  padding: 2rem;
+  color: $text-main-color;
 
-  .job-manage-wrapper {
-    width: 90%;
-    height: calc(90vh - 5rem);
-    border-radius: 20px;
-    margin: 2rem auto 0 auto;
-    background-color: #ffffff;
-    border-bottom: solid 1px $card-border-color;
-    border-right: solid 1px $card-border-color;
-    border-left: solid 1px $card-border-color;
-    position: relative;
-    font-size: 14px;
+  @media screen and (max-width: 1000px) {
+    width: 50%;
+  }
 
-    .manage-job-area {
-      background-color: #606060;
+  @media screen and (max-width: 900px) {
+    width: 85%;
+  }
+
+  @media screen and (max-width: 600px) {
+    width: 95%;
+  }
+
+  @media screen and (max-width: 450px) {
+    width: 98%;
+    padding: 1rem;
+  }
+
+  &__header {
+    .router-link {
+      color: $text-main-color;
+      text-decoration: none;
       width: 33.3%;
-      padding: 1rem 0;
-      border-radius: 20px 0 0 0;
-      display: inline-block;
-      color: $basic-white;
-      font-weight: bold;
+      padding: 0.7rem 0;
+      border-bottom: grey 1px solid;
     }
 
-    .apply-job-area {
-      background-color: #606060;
-      width: 33.4%;
-      // height: calc(68px - 1.6rem);
-      padding: 1rem 0;
-      display: inline-block;
-      color: $basic-white;
-      border: 0.5px solid $basic-white;
+    .router-link-active-click {
       font-weight: bold;
-      transition: .3s;
-    }
-
-    .reject-job-area {
-      @include box-shadow-manage;
-      background-color: $tertiary-color;
+      color: $text-main-color;
+      text-decoration: none;
       width: 33.3%;
-      // height: calc(68px - 1.6rem);
-      padding: 1rem 0;
-      border-radius: 0 20px 0 0;
-      display: inline-block;
-      color: $basic-white;
-      font-weight: bold;
-      transition: .3s;
-    }
-  }
-
-  .title-area {
-    width: 33.3%;
-    // height: calc(48px - 1.8rem);
-    padding: 0.8rem 0;
-    background-color: $tertiary-color;
-    display: inline-block;
-    color: $basic-white;
-    font-weight: bold;
-  }
-
-  .time-area {
-    width: 33.4%;
-    // height: calc(48px - 1.8rem);
-    padding: 0.8rem 0;
-    border-left: 1px solid $basic-white;
-    border-right: 1px solid $basic-white;
-    background-color: $tertiary-color;
-    display: inline-block;
-    color: $basic-white;
-    font-weight: bold;
-  }
-
-  .skill-area {
-    width: 33.3%;
-    // height: calc(48px - 1.8rem);
-    padding: 0.8rem 0;
-    background-color: $tertiary-color;
-    display: inline-block;
-    color: $basic-white;
-    font-weight: bold;
-  }
-
-  .job-title-area {
-    width: 33.3%;
-    height: 70%;
-    display: inline-block;
-  }
-
-  .router :hover {
-    background-color: $manage-hover-color;
-  }
-
-  .job-area {
-    transition: .2s;
-    // border-bottom: 1px solid #9c9c9c;
-  }
-
-  .job-wrapper-area {
-    width: 100%;
-    height: 81%;
-    overflow: scroll;
-
-    .job-area {
-      line-height: 2;
-
-      .job-area-box {
-        width: 33.2%;
-        border-bottom: 1px solid #9c9c9c;
-        // height: calc(48px - 1.6rem);
-        padding: 0.8rem 0;
-        color: $text-main-color;
-        font-size: 14px;
-        display: inline-block;
-        pointer-events: none;
-        line-height: 2;
-
-        p{
-          display: none;
-        }
-      }
-
-      .lang {
-        display: inline-block;
-        }
+      padding: 0.7rem 0;
+      border-bottom: grey 1px solid;
+      background-color: silver;
     }
   }
 }
 
-@media screen and (max-width: 1200px) {
-  .manage-wrapper {
-    margin: 0rem auto;
-    width: 90%;
-  
-    .job-manage-wrapper {
-      width: 93%;
-      border-radius: 20px;
-
-      .job-area {
-        display:flex;
-      }
-    }
-  }
-}
-
-/* タブレット */
-@media screen and (max-width: 900px) {
-  .manage-wrapper {
-    margin: 0rem auto;
-
-    .job-manage-wrapper {
-      width: 96%;
-    }
-  }
-}
-
-@media screen and (max-width: 768px) {
-  .manage-wrapper {
-    .job-manage-wrapper{
-      .manage-job-area {
-        width: 33.1%;
-        padding: 0.8rem 0;
-      }
-
-      .apply-job-area {
-        width: 33.1%;
-        padding: 0.8rem 0;
-      }
-
-      .reject-job-area {
-        width: 33.1%;
-        padding: 0.8rem 0;
-      }
-
-      .title-area {
-        width: 33.1%;
-        padding: 0.8rem 0;
-        background-color: $tertiary-color;
-        display: inline-block;
-        color: $basic-white;
-        font-weight: bold;
-      } 
-
-      .time-area{
-        width: 33.1%;
-        padding: 0.8rem 0;
-      }
-
-      .skill-area {
-        width: 33.1%;
-        padding: 0.8rem 0;
-      }
-    }
-  }
-}
-
-
-/*スマホ*/
-@media screen and (max-width: 500px){
-  .manage-wrapper {
-    width: 100%;
-    height: 89.5vh;
-    margin: 0 auto;
-
-    .job-manage-wrapper {
-      position: relative;
-      font-size: 14px;
-    }
-    .job-wrapper-area .job-area .job-area-box{
-      p{
-        display: inline;
-      }
-      span{
-        display: none;
-      } 
-    }
-  } 
-}
-
-@media (max-width: 420px){
-  .manage-wrapper {
-    width: 100%;
-    height: 89.5vh;
-    margin: 0 auto;
-
-    .job-manage-wrapper {
-      width: 97%;
-      margin: 0 auto;
-      margin: 2rem auto;
-      position: relative;
-    }
-
-  .manage-job-area {
-    width: 33%;
-    padding: 0.8rem 0;
-  }
-
-    .apply-job-area {
-      width: 33%;
-      padding: 0.8rem 0;
-    }
-
-    .reject-job-area {
-      width: 33%;
-      padding: 0.8rem 0;
-    }
-
-    .title-area {
-      width: 33%;
-      padding: 0.8rem 0;
-      background-color: $tertiary-color;
-      display: inline-block;
-      color: $basic-white;
-      font-weight: bold;
-  }
-
-    .time-area{
-      width: 33;
-      padding: 0.8rem 0;
-    }
-
-    .skill-area {
-      width: 33%;
-      padding: 0.8rem 0;
-    }
-    .job-wrapper-area {
-      width: 100%;
-      height: 81%;
-      overflow: scroll;
-
-      .job-area {
-        display:flex;
-
-        .job-area-box {
-          width: 33.2%;
-          height: 100%;
-          padding: 0.8rem 0;
-          color: $text-main-color;
-          font-size: 14px;
-          display: inline-block;
-          pointer-events: none;
-
-          p{
-            display: inline;
-          }
-          span{
-            display: none;
-          }
-        }
-      }
-    }
-  }
+.users {
+  text-decoration: none;
 }
 
 </style>
